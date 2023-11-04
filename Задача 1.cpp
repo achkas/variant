098fa4ba82280207039787b0d08fa9006b93ccca
Igor Achkas <jigoren@yandex.ru>
2023-11-04 15:08:48 +0700

    Задача 1. Частота символов

--- Задача 1.cpp
+++ Задача 1.cpp
@@ -5,39 +5,12 @@
 #include <windows.h>
 #include <vector>
 #include <algorithm>
+#include <array>
+#include <set>
+#include <map>
+#include <unordered_map>
 
-class simple_functor {
 
-    int sum;
-    int count;
-
-public:
-
-    simple_functor() : sum{ 0 }, count{ 0 } {};
-
-    int operator()(int number)
-    {
-        std::cout << number << ' ';
-
-        if (number % 3 == 0)
-        {
-            sum = sum + number;
-            count++;
-
-            return sum;
-        }
-    }
-
-    int get_count() { return count; }
-
-    int get_sum() { return sum; }
-
-    void PrintIntRES()
-
-    {
-        std::cout << "[OUT] : get_sum() = " << sum << std::endl << "[OUT] : get_count() = " << count;
-    }
-};
 
 
 
@@ -49,21 +22,51 @@ int main()
 
            
 
-        simple_functor sf;
+    std::cout << "[IN]: " << "Hello world!!" << std::endl;
+    std::cout << std::endl;
 
-        std::vector<int> vec{ 4, 1, 3, 6, 25, 54 };
-      
-        std::cout << "[IN] : ";
-        sf = std::for_each(vec.begin(), vec.end(), simple_functor());
-       
-        std::cout << std::endl;      
-        
-        std::cout << "[OUT] : get_sum() = " << sf.get_sum() << std::endl 
-            << "[OUT] : get_count() = " << sf.get_count();
-       
-        
-      
-    
+
+    typedef std::pair<std::string, int> pair;
+    std::vector<pair> vec;
+    std::map< std::string, int>letter_key;
+    std::multimap< std::string, int>letter2_key
+    {
+        {"H", 1}, { "e",1 }, { "l",1 }, { "l",1 }, { "o",1 }, { " ",1 },
+        { "w",1 }, { "o",1 }, { "r",1 }, { "l",1 }, { "d",1 }, { "!",1 }, { "!",1 }
+    };
+
+
+
+    for (auto iter{ letter2_key.begin() }; iter != letter2_key.end(); iter++)
+    {        
+        letter_key[iter->first] = letter2_key.count(iter->first);        
+    }
+
+    /*for (const auto& elem : letter_key)
+        std::cout << elem.first << ": " << elem.second << std::endl;
+        std::cout << std::endl;*/
+
+    std::copy(letter_key.begin(), letter_key.end(), std::back_inserter<std::vector<pair>>(vec));
+
+    std::sort(vec.begin(), vec.end(), [](const pair& l, const pair& v)
+        {
+            if (l.second != v.second)
+            {
+                return l.second > v.second;
+            }
+            return l.first > v.first;
+        }
+    );
+
+    /*for (auto const& pair : vec)
+    {
+     std::cout << '{' << pair.first << "," << pair.second << '}' << std::endl;
+    }*/
+
+    std::cout << "[OUT]: " << std::endl;
+
+    for (const auto& elem : vec)
+        std::cout << elem.first << ": " << elem.second << std::endl;
     
     return 0;
 }
