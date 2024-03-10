c87df9a4a8713fd375e5be78e9a12c081ac030ff
Igor Achkas <jigoren@yandex.ru>
2024-03-10 14:12:08 +0700

    Сортировка выбором исправлено

--- Задача 1.cpp
+++ Задача 1.cpp
@@ -7,56 +7,58 @@
 #include <windows.h>
 #include<algorithm>
 #include <future>
-		using namespace std::chrono_literals;
+using namespace std::chrono_literals;
 
-		void printArray(int arr[], int size)
-		{
-			int i;
-			for (i = 0; i < size; i++)
-				std::cout << arr[i] << " ";
-			std::cout << std::endl;
-		}
+void printArray(int arr[], int size)
+{
+	int i;
+	for (i = 0; i < size; i++)
+		std::cout << arr[i] << " ";
+	std::cout << std::endl;
+}
 
 std::once_flag flag;
 void print_once()
 {
-	std::cout << "id = " << std::this_thread::get_id() << ": ";
+	std::cout << "id_srch = " << std::this_thread::get_id() << ": ";
 }
 
 void srch(int arr[], int size, int i, std::promise<int> prms)
-{	
+{
 	std::call_once(flag, print_once);
 	int min = i;
 	for (int j = i + 1; j < size; j++)
-	min = (arr[j] < arr[min]) ? j : min;	
+		min = (arr[j] < arr[min]) ? j : min;
 	prms.set_value(min);
 
-}	
+}
+
+void sort_arr(int arr[], int size)
+{	
+	for (int i = 0; i < size; i++)
+	{
+		std::promise<int> prms;
+		std::future<int> ft = prms.get_future();
+		auto res1 = std::async(srch, arr, size, i, move(prms));
+		std::swap(arr[i], arr[ft.get()]);	
+
+	}	
+
+}
 
 
-			int main()
-			{
-				setlocale(LC_ALL, "ru");
-				SetConsoleCP(1251);
-				SetConsoleOutputCP(1251);
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 
-				std::cout << "id = " << std::this_thread::get_id() << ": ";
-				const int size = 10;
-				int arr[size] = { 14, 25, 6, 2, 43, 5, 96, 3, 4, 13 };
-				printArray(arr, size);
+	std::cout << "id_main = " << std::this_thread::get_id() << ": ";
+	const int size = 10;
+	int arr[size] = { 14, 25, 6, 2, 43, 5, 96, 3, 4, 13 };
+	printArray(arr, size);
+	sort_arr(arr, size);	
+	printArray(arr, size);
 
-				
-				
-				int min = 0; 				
-				for (int i = 0; i < size; i++)
-				{	
-					std::promise<int> prms;
-				std::future<int> ft  = prms.get_future();
-					auto res1 = std::async(srch, arr, size, i, move(prms));					
-				std::swap(arr[i], arr[ft.get()]);
-						
-				}
-				printArray(arr, size);
-				
-		 return 0;
-			}
+	return 0;
+}
