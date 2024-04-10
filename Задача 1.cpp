a5cf53e22161d3eca8f41debb32507edac966c00
Igor Achkas <jigoren@yandex.ru>
2024-04-10 15:15:44 +0700

    Задание 1: класс SqlSelectQueryBuilder

--- Задача 1.cpp
+++ Задача 1.cpp
@@ -1,64 +1,96 @@
 ﻿// Задача 1.
 
-#include <iostream>
-#include<thread>
-#include <chrono>
-#include <string>
-#include <windows.h>
-#include<algorithm>
-#include <future>
-using namespace std::chrono_literals;
+#include "map"
+#include "vector"
+#include "chrono"
+#include "iostream"
 
-void printArray(int arr[], int size)
+struct Query {
+    std::string name;
+    std::string from;
+    std::string where;
+    std::vector<std::string> query_V{ "SELECT " };
+    std::string all{ "*...; " };
+    std::string and_{ "AND " };
+    std::string end_Query{ ";" };
+    int w_call{ 0 };
+    
+};
+
+
+
+class QueryBuilder {
+public:
+
+
+    Query Buildquery() noexcept
+    {
+        if (event.name.empty())
+        {
+            event.query_V.push_back(event.all);
+            return event;
+        }
+        event.query_V.push_back(event.name);
+        event.query_V.push_back(event.from);
+        event.query_V.push_back(event.where);
+        event.query_V.push_back(event.end_Query);       
+        return event;
+    }
+
+    QueryBuilder& AddColumn(std::string column) noexcept {
+        event.name = column + " ";               
+        return *this;
+    }
+
+    QueryBuilder& AddFrom(std::string column) noexcept {
+        event.from = "FROM " + column + " ";             
+        return *this;
+    }
+
+    QueryBuilder& AddWhere(std::string id, std::string num) noexcept
+    {
+        if (event.w_call == 0)
+        {
+            event.where = "WHERE " + id + "=" + num + " ";
+        }
+        else
+        {
+            event.where += event.and_ + id + "=" + num + " ";
+        }        
+        event.w_call++;
+        return *this;
+    }
+   
+private:
+    Query event;
+};
+
+
+
+void Printquery(const Query& e)
 {
-	int i;
-	for (i = 0; i < size; i++)
-		std::cout << arr[i] << " ";
-	std::cout << std::endl;
+    for (const auto& query : e.query_V)
+    {
+        std::cout << query;
+    }   
+
 }
 
-std::once_flag flag;
-void print_once()
-{
-	std::cout << "id_srch = " << std::this_thread::get_id() << ": ";
-}
+int main() {
 
-void srch(int arr[], int size, int i, std::promise<int> prms)
-{
-	std::call_once(flag, print_once);
-	int min = i;
-	for (int j = i + 1; j < size; j++)
-		min = (arr[j] < arr[min]) ? j : min;
-	prms.set_value(min);
+    QueryBuilder a;
+       a.AddColumn("1wert, 2ropu")
+        .AddFrom("cok")
+        .AddWhere("id", "50")
+        .AddWhere("name", "John");
+    Printquery(a.Buildquery());
 
-}
+    std::cout << std::endl;
 
-void sort_arr(int arr[], int size)
-{	
-	for (int i = 0; i < size; i++)
-	{
-		std::promise<int> prms;
-		std::future<int> ft = prms.get_future();
-		auto res1 = std::async(srch, arr, size, i, move(prms));
-		std::swap(arr[i], arr[ft.get()]);	
-
-	}	
-
-}
-
-
-int main()
-{
-	setlocale(LC_ALL, "ru");
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-
-	std::cout << "id_main = " << std::this_thread::get_id() << ": ";
-	const int size = 10;
-	int arr[size] = { 14, 25, 6, 2, 43, 5, 96, 3, 4, 13 };
-	printArray(arr, size);
-	sort_arr(arr, size);	
-	printArray(arr, size);
-
-	return 0;
+    QueryBuilder b;
+    /*b.AddFrom("ERT")
+        .AddWhere("id", "90")
+        .AddWhere("name", "Karl");*/
+    Printquery(b.Buildquery());
+        
 }
